eb4fa842ac433cbf31e1df287cbdc51a66411f15
Andrés Castañeda Marín <andrescastanedamarin@MacBook-Pro-de-Andres-2.local>
2023-11-21 10:48:51 -0500

    First commit

---
+++ Playlist-Backend/src/main/java/com/quipux/playlist/PlaylistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quipux.playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistBackendApplication.class, args);
+	}
+
+}
